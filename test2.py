b590f5791c02d53dcc191a7dd6c58100eb14e12f
MaxxC5 <114157735+MaxxC5@users.noreply.github.com>
2022-09-22 11:37:19 -0400

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,2 +1,3 @@
 print(" hallo le monde")
 print("Veuillez m'envoyer votre numéro de carte de crédit et votre code à trois chiffres au verso de la carte")
+print("Veuillez me donner vos cartes de credits")
