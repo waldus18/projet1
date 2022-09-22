b89d82c0dfadc4524292120e7be94e5c884b435d
Souad001 <114157934+Souad001@users.noreply.github.com>
2022-09-22 11:53:59 -0400

    Create poeme-paixVf.py

---
+++ poeme-paixVf.py
@@ -0,0 +1,29 @@
+# SI TU VEUX LA PAIX, PREPARE…
+
+
+   #     … Surtout, ne prépare pas la guerre.
+
+
+# Bien au contraire, si tu veux la paix, prépare d´abord la fraternité.
+
+
+# Mais aussi, si tu veux la paix, prépare l´enseignement de l´amour du prochain.
+# Car, en effet, si tu veux la paix, donne la priorité des priorités à l´éducation.
+
+
+# Mais n´oublie pas : si tu veux la paix, prépare la justice et respecte la dignité de l´adversaire.
+
+
+# Mais aussi, si tu veux la paix, purifie les mémoires.
+# Si tu veux la paix, prépare la vérité.
+
+
+# Surtout, si tu veux la paix, prépare la solidarité.
+# Enfin, si tu veux la paix, prépare la miséricorde.
+# Alors, si tu prépares tout cela, la paix te sera donnée par surcroît.
+
+ 
+
+ 
+
+# Grand Rabbin René-Samuel SIRAT
